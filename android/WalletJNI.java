c96cffef7278be0e3b49d1a90e6d48b964d442ac
Vadim Grigoruk <vadim@beam-mw.com>
2018-09-28 15:22:27 +0300

    android wip

---
+++ android/WalletJNI.java
@@ -0,0 +1,16 @@
+public class WalletJNI
+{
+	public native int createWallet(String dbName, String pass, String seed);
+	public native int openWallet(String dbName, String pass);
+	public native boolean isWalletInitialized(String dbName);
+	public native void closeWallet(int wallet);
+
+	static {
+	//    System.loadLibrary("libwallet-jni");
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello world");
+	}
+}
