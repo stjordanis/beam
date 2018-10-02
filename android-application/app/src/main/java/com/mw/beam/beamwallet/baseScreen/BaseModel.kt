e98827f82d521e989b5ad77fff423afc2ed81921
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-02 12:24:18 +0300

    initial commit android application

---
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/baseScreen/BaseModel.kt
@@ -0,0 +1,7 @@
+package com.mw.beam.beamwallet.baseScreen
+
+/**
+ * Created by vain onnellinen on 10/1/18.
+ */
+open class BaseModel {
+}
---
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/baseScreen/BasePresenter.kt
@@ -0,0 +1,15 @@
+package com.mw.beam.beamwallet.baseScreen
+
+/**
+ * Created by vain onnellinen on 10/1/18.
+ */
+abstract class BasePresenter<T : MvpView>(var view : T?) : MvpPresenter<T> {
+
+    override fun detachView() {
+        view = null
+    }
+
+    protected fun isViewAttached(): Boolean {
+        return view != null
+    }
+}
