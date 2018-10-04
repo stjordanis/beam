f8452a99bc212b877f86367ed3b3df8c8066e28c
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-04 12:21:59 +0300

    refactoring. added WalletActivity, navDrawer, implemented walletInfo

---
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/wallet/WalletModel.kt
@@ -0,0 +1,11 @@
+package com.mw.beam.beamwallet.wallet
+
+import com.mw.beam.beamwallet.baseScreen.BaseModel
+
+/**
+ * Created by vain onnellinen on 10/1/18.
+ */
+class WalletModel : BaseModel() {
+    val available : Long
+    get() = 0
+}
