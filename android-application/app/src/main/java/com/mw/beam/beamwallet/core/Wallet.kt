631d9c8e2d489d11ce910789cea838f9c236cafb
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-04 12:21:59 +0300

    refactoring. added WalletActivity, navDrawer, implemented walletInfo

---
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/core/Wallet.kt
@@ -0,0 +1,12 @@
+package com.mw.beam.beamwallet.core
+
+/**
+ * Created by vain onnellinen on 10/2/18.
+ */
+data class Wallet(val _this: Long) {
+    external fun closeWallet()
+    external fun changeWalletPassword(pass: String)
+    external fun getSystemState(): SystemState
+    external fun getUtxos(): Array<Utxo>
+    external fun getTxHistory(): Array<TxDescription>
+}
