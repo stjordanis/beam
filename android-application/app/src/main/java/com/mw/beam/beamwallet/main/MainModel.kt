1ee8e0e24691b55e4018e32c3b30adc582021d5c
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-04 12:21:59 +0300

    refactoring. added WalletActivity, navDrawer, implemented walletInfo

--- android-application/app/src/main/java/com/mw/beam/beamwallet/main/MainModel.kt
+++
@@ -1,8 +0,0 @@
-package com.mw.beam.beamwallet.main
-
-import com.mw.beam.beamwallet.baseScreen.BaseModel
-
-/**
- * Created by vain onnellinen on 10/1/18.
- */
-class MainModel : BaseModel()
---
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/utxo/UtxoActivity.kt
@@ -0,0 +1,27 @@
+package com.mw.beam.beamwallet.utxo
+
+import android.os.Bundle
+import com.mw.beam.beamwallet.R
+import com.mw.beam.beamwallet.baseScreen.BaseActivity
+import com.mw.beam.beamwallet.core.Wallet
+import kotlinx.android.synthetic.main.activity_utxo.*
+
+/**
+ * Created by vain onnellinen on 10/2/18.
+ */
+class UtxoActivity : BaseActivity<UtxoPresenter>(), UtxoContract.View {
+    private lateinit var presenter: UtxoPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_utxo)
+
+        presenter = UtxoPresenter(this, UtxoModel())
+        configPresenter(presenter)
+    }
+
+    override fun configData(wallet: Wallet) {
+        height.text = wallet.getSystemState().height.toString()
+        hash.text = wallet.getSystemState().hash.toString()
+    }
+}
