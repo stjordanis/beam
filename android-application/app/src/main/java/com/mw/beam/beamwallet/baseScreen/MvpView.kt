14be52a61635687ea6e90de328ea7a299989868d
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-04 12:21:59 +0300

    refactoring. added WalletActivity, navDrawer, implemented walletInfo

--- android-application/app/src/main/java/com/mw/beam/beamwallet/baseScreen/MvpView.kt
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/baseScreen/MvpView.kt
@@ -1,11 +1,12 @@
 package com.mw.beam.beamwallet.baseScreen
 
-import com.mw.beam.beamwallet.core.ApiConfig
+import com.mw.beam.beamwallet.core.AppConfig
 
 /**
  * Created by vain onnellinen on 10/1/18.
  */
 interface MvpView {
-    fun showSnackBar(status : ApiConfig.Status)
+    fun showSnackBar(status : AppConfig.Status)
     fun hideKeyboard()
+    fun configNavDrawer()
 }
