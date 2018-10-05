0d1f5de61e88f7be834c6209c1c3a487ba9f2b80
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-05 18:02:42 +0300

    handled notification from library added observables for background async work

--- android-application/app/src/main/java/com/mw/beam/beamwallet/core/ApiListeners.kt
+++
@@ -1,7 +0,0 @@
-package com.mw.beam.beamwallet.core
-
-/**
- * Created by vain onnellinen on 10/4/18.
- */
-object ApiListeners {
-}
--- android-application/app/src/main/java/com/mw/beam/beamwallet/openWallet/OpenWalletModel.kt
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/openWallet/OpenWalletModel.kt
@@ -2,8 +2,9 @@ package com.mw.beam.beamwallet.openWallet
 
 import com.mw.beam.beamwallet.baseScreen.BaseModel
 import com.mw.beam.beamwallet.core.Api
-import com.mw.beam.beamwallet.core.AppConfig
 import com.mw.beam.beamwallet.core.App
+import com.mw.beam.beamwallet.core.AppConfig
+import io.reactivex.Observable
 
 /**
  * Created by vain onnellinen on 10/1/18.
@@ -23,12 +24,7 @@ class OpenWalletModel : BaseModel() {
         }
     }
 
-    fun openWallet(pass: String?): AppConfig.Status {
-        return if (pass.isNullOrBlank()) {
-            AppConfig.Status.STATUS_ERROR
-        } else {
-            App.wallet = Api.openWallet(AppConfig.DB_PATH, pass!!)
-            return AppConfig.Status.STATUS_OK
-        }
+    fun openWallet(pass: String?): Observable<AppConfig.Status> {
+        return createObservable { App.wallet = Api.openWallet(AppConfig.DB_PATH, pass!!) }
     }
 }
