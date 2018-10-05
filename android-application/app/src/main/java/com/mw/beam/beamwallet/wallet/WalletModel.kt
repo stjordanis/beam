f7f64b3e900b5fa980ddc994fa88046ef38f921a
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-05 18:02:42 +0300

    handled notification from library added observables for background async work

--- android-application/app/src/main/java/com/mw/beam/beamwallet/wallet/WalletModel.kt
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/wallet/WalletModel.kt
@@ -1,11 +1,14 @@
 package com.mw.beam.beamwallet.wallet
 
 import com.mw.beam.beamwallet.baseScreen.BaseModel
+import com.mw.beam.beamwallet.core.entities.TxDescription
+import io.reactivex.Observable
 
 /**
  * Created by vain onnellinen on 10/1/18.
  */
 class WalletModel : BaseModel() {
-    val available : Long
-    get() = 0
+
+    fun getTxHistory(): Observable<Array<TxDescription>> = createObservable { wallet.getTxHistory() }
+    fun getAvailable(): Observable<Long> = createObservable { wallet.getAvailable() }
 }
