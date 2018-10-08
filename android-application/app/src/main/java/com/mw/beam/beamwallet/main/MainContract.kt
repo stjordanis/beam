6fd9a4a2298873ce8220e3b9cf2d201284faa024
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-08 14:21:31 +0300

    added contract between presenter and repository

--- android-application/app/src/main/java/com/mw/beam/beamwallet/main/MainContract.kt
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/main/MainContract.kt
@@ -1,15 +1,14 @@
 package com.mw.beam.beamwallet.main
 
 import com.mw.beam.beamwallet.baseScreen.MvpPresenter
+import com.mw.beam.beamwallet.baseScreen.MvpRepository
 import com.mw.beam.beamwallet.baseScreen.MvpView
 
 /**
  * Created by vain onnellinen on 10/4/18.
  */
 interface MainContract {
-    interface View : MvpView {
-    }
-
-    interface Presenter : MvpPresenter<View> {
-    }
+    interface View : MvpView
+    interface Presenter : MvpPresenter<View>
+    interface Repository : MvpRepository
 }
