cf8dd2544b6c4ffae7b9996a0a93bf81c3d3d3a9
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-08 14:21:31 +0300

    added contract between presenter and repository

---
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/baseScreen/MvpRepository.kt
@@ -0,0 +1,10 @@
+package com.mw.beam.beamwallet.baseScreen
+
+import com.mw.beam.beamwallet.core.entities.Wallet
+
+/**
+ * Created by vain onnellinen on 10/8/18.
+ */
+interface MvpRepository {
+    fun getWallet() : Wallet
+}
