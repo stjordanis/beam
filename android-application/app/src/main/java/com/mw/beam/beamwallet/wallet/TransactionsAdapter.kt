afd762c100e9b3cd8af5f6739ee7ed7d0cc721f1
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-17 15:59:21 +0300

    added menu for transactions, changed some elements at WalletFragment, added communication with presenter for future logic

--- android-application/app/src/main/java/com/mw/beam/beamwallet/wallet/TransactionsAdapter.kt
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/wallet/TransactionsAdapter.kt
@@ -16,17 +16,21 @@ import kotlinx.android.synthetic.main.item_transaction.*
 /**
  * Created by vain onnellinen on 10/2/18.
  */
-class TransactionsAdapter(private val context: Context, private var data: Array<TxDescription>, private val clickListener: OnItemClickListener) :
+class TransactionsAdapter(private val context: Context, private var data: List<TxDescription>, private val clickListener: OnItemClickListener) :
         RecyclerView.Adapter<TransactionsAdapter.ViewHolder>() {
     private val beamResId = R.drawable.ic_beam
     private val sentCurrencyResId = R.drawable.beam_sent
     private val receivedCurrencyResId = R.drawable.beam_received
     private val receivedColor = ContextCompat.getColor(context, R.color.received_color)
-    private val receivedText = context.getString(R.string.wallet_status_received)
+    private val receivedStatus = context.getString(R.string.wallet_status_received)
     private val sentColor = ContextCompat.getColor(context, R.color.sent_color)
-    private val sentText = context.getString(R.string.wallet_status_sent)
+    private val sentStatus = context.getString(R.string.wallet_status_sent)
+    private val commonStatusColor = ContextCompat.getColor(context, R.color.transaction_common_status_color)
     private val multiplyColor = ContextCompat.getColor(context, R.color.wallet_adapter_multiply_color)
     private val notMultiplyColor = ContextCompat.getColor(context, R.color.wallet_adapter_not_multiply_color)
+    private val receiveText = context.getString(R.string.wallet_transactions_receive)
+    private val sendText = context.getString(R.string.wallet_transactions_send)
+    private val swapText = context.getString(R.string.wallet_transactions_swap)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_transaction, parent, false))
 
@@ -34,44 +38,45 @@ class TransactionsAdapter(private val context: Context, private var data: Array<
         val transaction = data[position]
 
         holder.apply {
-            when (EntitiesHelper.TxSender.fromValue(transaction.sender)) {
+            when (transaction.senderEnum) {
                 EntitiesHelper.TxSender.RECEIVED -> {
                     sum.setTextColor(receivedColor)
                     status.setTextColor(receivedColor)
-                    status.text = receivedText
+                    status.text = receivedStatus
                     currency.setImageResource(receivedCurrencyResId)
+                    message.text = String.format(receiveText, "BEAM") //TODO replace when multiply currency will be available
                 }
                 EntitiesHelper.TxSender.SENT -> {
                     sum.setTextColor(sentColor)
                     status.setTextColor(sentColor)
-                    status.text = sentText
+                    status.text = sentStatus
                     currency.setImageResource(sentCurrencyResId)
+                    message.text = String.format(sendText, "BEAM") //TODO replace when multiply currency will be available
                 }
             }
+
+            if (transaction.statusEnum == EntitiesHelper.TxStatus.Failed
+                    || transaction.statusEnum == EntitiesHelper.TxStatus.InProgress
+                    || transaction.statusEnum == EntitiesHelper.TxStatus.Cancelled) status.setTextColor(commonStatusColor)
 
             itemView.setBackgroundColor(if (position % 2 == 0) multiplyColor else notMultiplyColor)
             icon.setImageResource(beamResId)
             date.text = CalendarUtils.fromTimestamp(transaction.modifyTime * 1000)
-            sum.text = EntitiesHelper.convertToBeam(transaction.amount).toString()
-            message.visibility = if (transaction.message == null) View.GONE else View.VISIBLE
-
-            if (transaction.message != null) {
-                message.text = String(transaction.message)
-            }
+            sum.text = EntitiesHelper.convertToBeamWithSign(transaction.amount, transaction.sender)
+            status.text = transaction.statusEnum.name.toLowerCase()
         }
     }
 
     override fun getItemCount(): Int = data.size
     fun getPositionByItem(item: TxDescription) = data.indexOf(item)
 
-    fun setData(data: Array<TxDescription>) {
+    fun setData(data: List<TxDescription>) {
         this.data = data
         notifyDataSetChanged()
     }
 
     interface OnItemClickListener {
         fun onItemClick(item: TxDescription)
-        fun onSwipeClick(item: TxDescription)
     }
 
     class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
