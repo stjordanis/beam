012d2948f18fce54817138d56741ebe06d28baa4
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-17 11:15:02 +0300

    changed work with api, work on WalletFragment design

--- android-application/app/src/main/java/com/mw/beam/beamwallet/core/entities/Wallet.kt
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/core/entities/Wallet.kt
@@ -4,10 +4,21 @@ package com.mw.beam.beamwallet.core.entities
  * Created by vain onnellinen on 10/2/18.
  */
 data class Wallet(val _this: Long) {
-    external fun closeWallet()
-    external fun changeWalletPassword(pass: String)
-    external fun getSystemState(): SystemState
-    external fun getUtxos(): Array<Utxo>
-    external fun getTxHistory(): Array<TxDescription>
-    external fun getAvailable(): Long
+    external fun sendMoney()
+    external fun sendMoney2()
+    external fun syncWithNode()
+    external fun calcChange()
+    external fun getWalletStatus()
+    external fun getUtxosStatus()
+    external fun getAddresses()
+    external fun cancelTx()
+    external fun deleteTx()
+    external fun createNewAddress()
+    external fun generateNewWalletID()
+    external fun changeCurrentWalletIDs()
+    external fun deleteAddress()
+    external fun deleteOwnAddress()
+    external fun setNodeAddress()
+    external fun emergencyReset()
+    external fun changeWalletPassword()
 }
