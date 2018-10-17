e1e827147a31cc752a64a18a46a23e5cbba4f53e
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-17 15:59:21 +0300

    added menu for transactions, changed some elements at WalletFragment, added communication with presenter for future logic

--- android-application/app/src/main/java/com/mw/beam/beamwallet/core/entities/TxDescription.kt
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/core/entities/TxDescription.kt
@@ -1,18 +1,24 @@
 package com.mw.beam.beamwallet.core.entities
 
+import com.mw.beam.beamwallet.core.helpers.EntitiesHelper
+
 /**
  * Created by vain onnellinen on 10/2/18.
  */
-data class TxDescription(val id: ByteArray,
-                         val amount: Long,
-                         val fee: Long,
-                         val change: Long,
-                         val minHeight: Long,
-                         val peerId: ByteArray,
-                         val myId: ByteArray,
-                         val message: ByteArray?,
-                         val createTime: Long,
-                         val modifyTime: Long,
-                         val sender: Boolean,
-                         val status: Int) {
+class TxDescription(val id: ByteArray,
+                    val amount: Long,
+                    val fee: Long,
+                    val change: Long,
+                    val minHeight: Long,
+                    val peerId: ByteArray,
+                    val myId: ByteArray,
+                    val message: ByteArray?,
+                    val createTime: Long,
+                    val modifyTime: Long,
+                    val sender: Boolean,
+                    val status: Int) {
+    val senderEnum: EntitiesHelper.TxSender
+        get() = EntitiesHelper.TxSender.fromValue(sender)
+    val statusEnum: EntitiesHelper.TxStatus
+        get() = EntitiesHelper.TxStatus.fromValue(status)
 }
