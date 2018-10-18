6099fd45d53907d5a93cda729c05df64f2ab2af1
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-18 15:00:35 +0300

    changed work with navigationDrawer and toolbars

--- android-application/app/src/main/java/com/mw/beam/beamwallet/baseScreen/BaseActivity.kt
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/baseScreen/BaseActivity.kt
@@ -1,26 +1,19 @@
 package com.mw.beam.beamwallet.baseScreen
 
 import android.app.Activity
-import android.content.res.Configuration
-import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.support.v4.content.ContextCompat
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.view.Gravity
-import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import com.mw.beam.beamwallet.R
 import com.mw.beam.beamwallet.core.AppConfig
-import com.mw.beam.beamwallet.core.utils.LogUtils
-import com.mw.beam.beamwallet.wallet.WalletFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
@@ -28,20 +21,19 @@ import kotlinx.android.synthetic.main.activity_main.*
  */
 abstract class BaseActivity<T : BasePresenter<out MvpView>> : AppCompatActivity(), MvpView {
     private lateinit var presenter: T
-    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     fun configPresenter(presenter: T) {
         this.presenter = presenter
         this.presenter.viewIsReady()
     }
 
-    private fun showFragment(
+    protected fun showFragment(
             fragment: Fragment,
             tag: String,
             clearToTag: String?,
             clearInclusive: Boolean
     ) {
-        drawerLayout.closeDrawer(Gravity.START)
+        drawerLayout?.closeDrawer(Gravity.START)
         val fragmentManager = supportFragmentManager
         val currentFragment = supportFragmentManager.findFragmentById(R.id.container)
 
@@ -58,33 +50,6 @@ abstract class BaseActivity<T : BasePresenter<out MvpView>> : AppCompatActivity(
             transaction.addToBackStack(tag)
             transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
             transaction.commit()
-        }
-    }
-
-    override fun configNavDrawer() {
-        setSupportActionBar(toolbarLayout.findViewById(R.id.toolbar))
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbarLayout.findViewById(R.id.toolbar), R.string.common_drawer_open, R.string.common_drawer_close)
-        drawerLayout.addDrawerListener(drawerToggle)
-        drawerToggle.syncState()
-        navView.itemIconTintList = getColorStateList(R.color.menu_selector)
-
-        navView.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.nav_wallet -> showFragment(WalletFragment.newInstance(), WalletFragment.getFragmentTag(), null, true)
-                R.id.nav_address_book -> LogUtils.log("address_book")
-                R.id.nav_utxo -> LogUtils.log("utxo")
-                R.id.nav_dashboard -> LogUtils.log("dashboard")
-                R.id.nav_notifications -> LogUtils.log("notifications")
-                R.id.nav_help -> LogUtils.log("help")
-                R.id.nav_settings -> LogUtils.log("settings")
-            }
-            true
-        }
-
-        navView.apply {
-            setCheckedItem(R.id.nav_wallet)
-            menu.performIdentifierAction(R.id.nav_wallet, 0)
         }
     }
 
@@ -120,18 +85,7 @@ abstract class BaseActivity<T : BasePresenter<out MvpView>> : AppCompatActivity(
         super.onPause()
     }
 
-    override fun onDestroy() {
-        presenter.detachView()
-
-        super.onDestroy()
-    }
-
     override fun onBackPressed() {
-        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-            return
-        }
-
         if (supportFragmentManager.backStackEntryCount == 1) {
             finish()
             return
@@ -140,21 +94,18 @@ abstract class BaseActivity<T : BasePresenter<out MvpView>> : AppCompatActivity(
         super.onBackPressed()
     }
 
-    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onPostCreate(savedInstanceState, persistentState)
-        drawerToggle.syncState()
+    override fun onDestroy() {
+        presenter.detachView()
+
+        super.onDestroy()
     }
 
-    override fun onConfigurationChanged(newConfig: Configuration?) {
-        super.onConfigurationChanged(newConfig)
-        drawerToggle.onConfigurationChanged(newConfig)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true
+    fun initToolbar(toolbar: Toolbar, title : String) {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setTitle(title)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
         }
-
-        return super.onOptionsItemSelected(item)
     }
 }
