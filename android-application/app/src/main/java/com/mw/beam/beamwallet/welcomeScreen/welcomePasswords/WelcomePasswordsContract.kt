3dce6189bdd394e4a38e7487bca2c776a4a12e74
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-24 17:13:52 +0300

    some style fixes

--- android-application/app/src/main/java/com/mw/beam/beamwallet/welcomeScreen/welcomePasswords/WelcomePasswordsContract.kt
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/welcomeScreen/welcomePasswords/WelcomePasswordsContract.kt
@@ -3,15 +3,19 @@ package com.mw.beam.beamwallet.welcomeScreen.welcomePasswords
 import com.mw.beam.beamwallet.baseScreen.MvpPresenter
 import com.mw.beam.beamwallet.baseScreen.MvpRepository
 import com.mw.beam.beamwallet.baseScreen.MvpView
+import com.mw.beam.beamwallet.core.views.PasswordStrengthView
 
 /**
  * Created by vain onnellinen on 10/23/18.
  */
 interface WelcomePasswordsContract {
     interface View : MvpView {
+        fun hasErrors() : Boolean
+        fun setStrengthLevel(strength : PasswordStrengthView.Strength)
     }
 
     interface Presenter : MvpPresenter<View> {
+        fun onPassChanged(pass : String?)
     }
 
     interface Repository : MvpRepository
