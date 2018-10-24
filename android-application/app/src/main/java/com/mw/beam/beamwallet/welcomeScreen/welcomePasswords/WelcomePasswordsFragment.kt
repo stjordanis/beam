00733eddd52c9ac269fb9966264e1eb37b6ea3c0
Tatsiana Khvisiuk <khvisiuk_tatsiana@senla.eu>
2018-10-24 17:13:52 +0300

    some style fixes

--- android-application/app/src/main/java/com/mw/beam/beamwallet/welcomeScreen/welcomePasswords/WelcomePasswordsFragment.kt
+++ android-application/app/src/main/java/com/mw/beam/beamwallet/welcomeScreen/welcomePasswords/WelcomePasswordsFragment.kt
@@ -2,11 +2,15 @@ package com.mw.beam.beamwallet.welcomeScreen.welcomePasswords
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mw.beam.beamwallet.R
 import com.mw.beam.beamwallet.baseScreen.BaseFragment
+import com.mw.beam.beamwallet.core.views.PasswordStrengthView
+import com.mw.beam.beamwallet.core.watchers.TextWatcher
+import kotlinx.android.synthetic.main.fragment_welcome_passwords.*
 
 /**
  * Created by vain onnellinen on 10/23/18.
@@ -37,5 +41,45 @@ class WelcomePasswordsFragment : BaseFragment<WelcomePasswordsPresenter>(), Welc
         super.onViewCreated(view, savedInstanceState)
         presenter = WelcomePasswordsPresenter(this, WelcomePasswordsRepository())
         configPresenter(presenter)
+
+        pass.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(password: Editable?) {
+                presenter.onPassChanged(password?.toString())
+            }
+        })
+
+        confirmPass.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(password: Editable?) {
+                passMatchError.visibility = View.GONE
+            }
+        })
+    }
+
+    override fun hasErrors(): Boolean {
+        var hasErrors = false
+        pass.error = null
+        confirmPass.error = null
+        passMatchError.visibility = View.GONE
+
+        if (pass.text.isNullOrBlank()) {
+            pass.error = getString(R.string.open_wallet_no_text_error)
+            hasErrors = true
+        }
+
+        if (!pass.text.isNullOrBlank() && pass.text.toString() != confirmPass.text.toString()) {
+            passMatchError.visibility = View.VISIBLE
+            hasErrors = true
+        }
+
+        if (confirmPass.text.isNullOrBlank()) {
+            confirmPass.error = getString(R.string.open_wallet_no_text_error)
+            hasErrors = true
+        }
+
+        return hasErrors
+    }
+
+    override fun setStrengthLevel(strength: PasswordStrengthView.Strength) {
+        strengthView.strength = strength
     }
 }
